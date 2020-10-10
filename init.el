1eb2e151d8581641683a050f9217bb2d00102394
Matt Young <dev@mttyng.com>
2020-10-10 00:18:51 -0700

    keybindings etc

--- init.el
+++ init.el
@@ -11,7 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(treemacs magit evil-leader evil use-package)))
+ '(package-selected-packages
+   '(evil-magit ivy general treemacs magit evil-leader evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,15 +22,24 @@
 (eval-when-compile
   (require 'use-package))
 
+(use-package general
+  :config
+  (general-evil-setup)
+  (general-auto-unbind-keys))
+
 (use-package evil
   :ensure t
+  :after (general)
   :config
   (evil-mode 1)
+  (use-package evil-magit
+    :ensure t)
   (use-package evil-leader
     :ensure t
     :config
-    (global-evil-leader-mode t)
-    (evil-leader/set-leader "<SPC>")))
+    (global-evil-leader-mode t)))
+
+(add-hook 'prog-mode-hook (lambda () (setq indent-tabs-mode nil)))
 
 (use-package magit
   :ensure t)
@@ -47,3 +57,30 @@
   (:map global-map
 	("M-0" . treemacs-select-window)
 	("C-x t t" . treemacs)))
+
+(global-set-key (kbd "C-h C-f") #'find-function)
+(global-set-key (kbd "C-g") #'evil-force-normal-state)
+(use-package which-key
+  :ensure t
+  :config
+  (setq which-key-idle-delay 0.2)
+  (setq which-key-min-display-lines 3)
+  (which-key-mode))
+
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
+(general-create-definer my--leader-def
+  :prefix "SPC")
+
+(my--leader-def
+  :states 'normal
+  :keymaps 'override
+  ":" 'execute-extended-command
+  "g g" 'magit
+  "C-w" 'evil-delete-backward-word
+  "f s" 'save-buffer)
